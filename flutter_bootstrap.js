cc508c1864e689c3d4bac893442fee47708e8ce6
hazem <thehazemmahdy@gmail.com>
2025-10-16 22:35:39 +0300

    Built web
    Branch: master
    Commit: ca3e2f06620feedf3cda8e0c0e162bf9bba138ff (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "246851752"
+    serviceWorkerVersion: "4247405914"
   }
 });
